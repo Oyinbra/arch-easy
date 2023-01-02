8b76cb6350948cd0093414d4db31995d00d392b1
oyinbra <onyinbra@gmail.com>
2023-01-02 22:37:11 +0100

    updated

--- sda-format-mount.sh
+++ sda-format-mount.sh
@@ -1,7 +1,7 @@
 timedatectl set-ntp true
 loadkeys us
-pacman -Sy reflector
-reflector --country US --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
+# pacman -Sy reflector archlinux-keyring
+# reflector --country US --latest 6 --sort rate --save /etc/pacman.d/mirrorlist
 pacman -Sy
 
 lsblk
@@ -14,20 +14,24 @@ mount /dev/sda3 /mnt
 
 btrfs subvolume create /mnt/@
 btrfs subvolume create /mnt/@home
-btrfs subvolume create /mnt/@cache
-btrfs subvolume create /mnt/@log
+btrfs subvolume create /mnt/@var
+btrfs subvolume create /mnt/@opt
+btrfs subvolume create /mnt/@tmp
+btrfs subvolume create /mnt/@srv
 btrfs subvolume create /mnt/@.snapshots
 
 umount -l /mnt
 
 mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@ /dev/sda3 /mnt
 
-mkdir -p /mnt/{boot/efi,home,var/cache,var/log,.snapshots}
+mkdir -p /mnt/{boot/efi,home,var,opt,tmp,srv,.snapshots}
 
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@home /dev/sda3 /mnt/home
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@cache /dev/sda3 /mnt/var/cache
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@log /dev/sda3 /mnt/var/log
-mount -o noatime,compress=zstd:1,ssd,discard=async,space_cache=v2,subvol=@.snapshots /dev/sda3 /mnt/.snapshots
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@home /dev/sda3 /mnt/home
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@var /dev/sda3 /mnt/var
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@opt /dev/sda3 /mnt/opt
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@tmp /dev/sda3 /mnt/tmp
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@srv /dev/sda3 /mnt/srv
+mount -o noatime,compress=zstd,ssd,discard=async,space_cache=v2,subvol=@.snapshots /dev/sda3 /mnt/.snapshots
 
 mount /dev/sda1 /mnt/boot/efi
 
